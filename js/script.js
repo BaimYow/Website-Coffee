482f23ebc5fe5957528721714acf995b90f36a91
BaimYow <baimyow1999@gmail.com>
2024-05-26 23:59:20 +0700

    membuat kedai coffee

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//Togle Class Active
+const navbarNav = document.querySelector(".navbar-nav");
+//ketika hamburger menu diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik diluar side bar untuk mengilangkan nav
+
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
